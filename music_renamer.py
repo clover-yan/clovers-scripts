54cc7a39cf395c119b0e0392f483827c58ed449d
clover_yan <56215525+clover-yan@users.noreply.github.com>
2024-10-18 12:58:06 +0800

    refactor(music_renamer): Refactor code

--- music_renamer.py
+++ music_renamer.py
@@ -1,12 +1,8 @@
 import os
 from tinytag import TinyTag
 
-mp3s = []
 
-# get all mp3 filenames in current path
-for filename in os.listdir('.'):
-	if filename.endswith(('mp3', 'MP3', 'flac', 'FLAC')):
-		mp3s.append(filename)
+mp3s = [i for i in os.listdir('.') if i.lower().endswith(('mp3', 'flac'))]
 
 for mp3 in mp3s:
 	extension = mp3.split('.')[-1].lower()
@@ -16,20 +12,21 @@ for mp3 in mp3s:
 	# replace invalid characters
 	new_name = new_name.replace('/', '-').replace('\\', '-').replace(':', ' -').replace('*', '').replace('?', '').replace('"', "'").replace('<', '').replace('>', '').replace('|', '').replace('!', '')
 	# check if file already exists
-	if new_name.lower() != mp3.lower() and os.path.exists(new_name):
+	if new_name[:-len(extension)-1] != mp3[:-len(extension)-1] and os.path.exists(new_name):
 		i = 2
 		valid_name = new_name[:-len(extension)-1] + f' ({i}).{extension}'
-		while new_name.lower() != mp3.lower() and os.path.exists(valid_name):
+		while valid_name[:-len(extension)-1] != mp3[:-len(extension)-1] and os.path.exists(valid_name):
 			i += 1
 			valid_name = new_name[:-len(extension)-1] + f' ({i}).{extension}'
 		new_name = valid_name
-	os.rename(mp3, new_name)
-	# lrc file
-	try:
-		lrc = mp3[:-len(extension)] + 'lrc'
-		new_lrc = new_name[:-len(extension)] + 'lrc'
-		os.rename(lrc, new_lrc)
-	except:
-		pass
-	print('Renamed:', mp3, '->', new_name)
+	if new_name != mp3:
+		os.rename(mp3, new_name)
+		print('Renamed:', mp3, '->', new_name)
+		try:
+			lrc = mp3[:-len(extension)] + 'lrc'
+			new_lrc = new_name[:-len(extension)] + 'lrc'
+			os.rename(lrc, new_lrc)
+			print('  Renamed lyrics too')
+		except:
+			pass
 print('Done!')
