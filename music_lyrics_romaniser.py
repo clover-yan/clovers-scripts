490fc9944814faca35139de5a52abf476579ff77
Clover Yan <i@khyan.top>
2025-12-02 17:13:35 +0800

    feat(music_lyrics_romaniser): 增加歌词样式和仅转换选项

--- music_lyrics_romaniser.py
+++ music_lyrics_romaniser.py
@@ -5,10 +5,37 @@ from mutagen.id3 import ID3, USLT
 import argparse
 import logging
 
-parser = argparse.ArgumentParser(description='Romanise Japanese lyrics in mp3/flac files in a directory')
-parser.add_argument('directory', nargs='?', default='.', help='Directory containing music files (default: current directory)')
-parser.add_argument('-r', '--recursive', action='store_true', help='Recurse into subdirectories')
-parser.add_argument('--dry-run', action='store_true', help='Perform a dry run without saving changes')
+parser = argparse.ArgumentParser(
+    description='Romanise Japanese lyrics in mp3/flac files in a directory')
+parser.add_argument(
+    'directory',
+    nargs='?',
+    default='.',
+    help='Directory containing music files (default: current directory)')
+parser.add_argument('-r',
+                    '--recursive',
+                    action='store_true',
+                    help='Recurse into subdirectories')
+parser.add_argument(
+    '-s',
+    '--style',
+    dest='style',
+    type=str.lower,
+    choices=('salt', 'vanilla'),
+    default='salt',
+    help=
+    'Set the style when translations are available: salt (Salt Player, placing romaji above translations) or vanilla (Vanilla Music, placing romaji in the same line with translations) (default: salt)'
+)
+parser.add_argument(
+    '-c',
+    '--convert-only',
+    action='store_true',
+    help=
+    'Convert existing romaji lines to the selected style instead of generating new ones'
+)
+parser.add_argument('--dry-run',
+                    action='store_true',
+                    help='Perform a dry run without saving changes')
 args = parser.parse_args()
 
 dirpath = os.path.abspath(args.directory)
@@ -23,81 +50,106 @@ if args.recursive:
 					music_list.append(path)
 else:
 	music_list = [
-		os.path.join(dirpath, i)
-		for i in os.listdir(dirpath)
-		if i.lower().endswith(('mp3', 'flac')) and os.path.isfile(os.path.join(dirpath, i))
+	    os.path.join(dirpath, i) for i in os.listdir(dirpath)
+	    if i.lower().endswith((
+	        'mp3', 'flac')) and os.path.isfile(os.path.join(dirpath, i))
 	]
 
 kks = pykakasi.kakasi()
 
-# Logging setup
-logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s] [%(levelname)s] %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
 
+
+def dump_additional_lines(additional_lines, timestamp):
+	result = ''
+	if additional_lines:
+		if args.style == 'salt':
+			result += '\n'.join((timestamp + i) for i in additional_lines) + '\n'
+		else:
+			result += timestamp + '｜'.join(additional_lines) + '\n'
+		additional_lines.clear()
+	return result
+
+
 def romanise_lyrics(text):
-	if not any((ord('ぁ') <= ord(i) <= ord('ん') for i in text)):
+	if not args.convert_only and not any(
+	    (ord('ぁ') <= ord(i) <= ord('ん') for i in text)):
 		return text
 	result = ''
 	line_timestamp = ''
+	additional_lines = []
 	for line in text.split('\n'):
 		if ']' not in line:
+			result += dump_additional_lines(additional_lines, line_timestamp)
 			result += line + '\n'
 			continue
 		current_line_timestamp = line[:line.index(']') + 1]
+		line_content = line[line.index(']') + 1:]
 		if line_timestamp == current_line_timestamp:
-			result += line + '\n'
+			if args.style == 'salt' and args.convert_only:
+				additional_lines += line_content.split('｜')
+			else:
+				additional_lines.append(line_content)
 			continue
+		result += dump_additional_lines(additional_lines, line_timestamp)
 		line_timestamp = current_line_timestamp
-		line_lyrics = line[line.index(']') + 1:]
-		if not line_lyrics.strip():
-			result += line + '\n'
-			continue
-		line_romanised = []
-		for item in kks.convert(line_lyrics):
-			line_romanised.append(item['hepburn'])
+		if line_content.strip() and not args.convert_only:
+			line_romanised = []
+			for item in kks.convert(line_content):
+				line_romanised.append(item['hepburn'])
+			additional_lines.append(' '.join(line_romanised))
 		result += line + '\n'
-		result += line_timestamp + ' '.join(line_romanised) + '\n'
+	result += dump_additional_lines(additional_lines, line_timestamp)
 	return result
 
-for music in music_list:
-	ext = music.split('.')[-1].lower()
-	if ext == 'flac':
-		tags = FLAC(music)
-		if 'lyrics' not in tags:
-			logger.warning('No lyrics found in %s', music)
-			continue
-		lyrics = tags['lyrics'][0]
-		romanised_lyrics = romanise_lyrics(lyrics)
-		if args.dry_run:
-			logger.info('Should have processed %s', music)
-			logger.info('\n%s', romanised_lyrics)
-		else:
-			tags['lyrics'] = romanised_lyrics
-			tags.save()
-			logger.info('Processed %s', music)
-	elif ext == 'mp3':
-		tags = ID3(music)
-		uslt_tag_name = None
-		for i in tags.keys():
-			if i.startswith('USLT'):
-				uslt_tag_name = i
-				if uslt_tag_name == 'USLT::jpn':
-					break
-		else:
-			if uslt_tag_name is None:
+
+if __name__ == '__main__':
+	for music in music_list:
+		ext = music.split('.')[-1].lower()
+		if ext == 'flac':
+			tags = FLAC(music)
+			if 'lyrics' not in tags:
 				logger.warning('No lyrics found in %s', music)
 				continue
-		lyrics = tags.getall(uslt_tag_name)[0].text
-		romanised_lyrics = romanise_lyrics(lyrics)
-		if args.dry_run:
-			logger.info('Should have processed %s', music)
-			logger.info('\n%s', romanised_lyrics)
+			lyrics = tags['lyrics'][0]
+			romanised_lyrics = romanise_lyrics(lyrics)
+			if args.dry_run:
+				logger.info('Should have processed %s', music)
+				logger.info('\n%s', romanised_lyrics)
+			else:
+				tags['lyrics'] = romanised_lyrics
+				tags.save()
+				logger.info('Processed %s', music)
+		elif ext == 'mp3':
+			tags = ID3(music)
+			uslt_tag_name = None
+			for i in tags.keys():
+				if i.startswith('USLT'):
+					uslt_tag_name = i
+					if uslt_tag_name == 'USLT::jpn':
+						break
+			else:
+				if uslt_tag_name is None:
+					logger.warning('No lyrics found in %s', music)
+					continue
+			lyrics = tags.getall(uslt_tag_name)[0].text
+			romanised_lyrics = romanise_lyrics(lyrics)
+			if args.dry_run:
+				logger.info('Should have processed %s', music)
+				logger.info('\n%s', romanised_lyrics)
+			else:
+				tags.delall(uslt_tag_name)
+				tags.add(
+				    USLT(encoding=3,
+				         lang=uslt_tag_name.split('::')[-1],
+				         desc='',
+				         text=romanised_lyrics))
+				tags.save(music)
+				logger.info('Processed %s', music)
 		else:
-			tags.delall(uslt_tag_name)
-			tags.add(USLT(encoding=3, lang=uslt_tag_name.split('::')[-1], desc='', text=romanised_lyrics))
-			tags.save(music)
-			logger.info('Processed %s', music)
-	else:
-		logger.warning('Unsupported file format: %s', music)
+			logger.warning('Unsupported file format: %s', music)
 
-logger.info('All done!')
+	logger.info('All done!')
